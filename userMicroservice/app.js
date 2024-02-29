94d9fe1ec201d152c35d32919e932f607edf8a86
Sneha-Chakraborty <sneha.chakraborty237@gmail.com>
2024-02-29 22:41:02 +0530

    Initial Commit

---
+++ userMicroservice/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const passport = require("passport");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+if (!process.env.NODE_ENV === "test") {
+  app.use(morgan("dev"));
+}
+app.use(passport.initialize());
+
+const uri = process.env.ATLAS_URI;
+const testUri = process.env.ATLAS_URI_TEST;
+if (!process.env.NODE_ENV === "test") {
+  mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+} else {
+  mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+}
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const userRouter = require("./routes/users");
+
+app.use("/", userRouter);
+
+module.exports = app;
